36b182dc10013386a5aa8e43089a39dc029864f5
Dmitrii Vladimirov <dvlad7909@gmail.com>
2023-10-12 18:45:57 -0700

    python exercise

---
+++ python_beginning/2_modify_numbers.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+level = 99.9999
+print(f"Learning Python is fun\"' - {str(level)[:5]} %")
+print("Learning Python is fun\"' - {} %".format(str(level)[:5]))
+print("Learning Python is fun\"' - %s" % str(level)[:5], "%")
