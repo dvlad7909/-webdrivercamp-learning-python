b4d88bea83b89773fa4ae8f75d9eda8014a80ca0
Dmitrii Vladimirov <dvlad7909@gmail.com>
2023-10-18 16:30:39 -0700

    import & modules exercise

---
+++ python-modules/0_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    # ADD YOUR IMPORT HERE
+    from calculation import add, sub, mul, div
+
+    a = 4  # use this variable as a first argument to call a function
+    b = 2  # use this variable as a second argument to call a function
+
+    # ADD YOUR CODE HERE
+    print(add(a, b))
+    print(sub(a, b))
+    print(mul(a, b))
+    print(div(a, b))
